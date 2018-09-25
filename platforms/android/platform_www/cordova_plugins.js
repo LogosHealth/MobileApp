62b080dda7f0333cf57a00a00fb589dfe1d2ba23
LogosHealth <Healthlogos@gmail.com>
2018-09-25 11:20:52 -0700

    ignoring platforms

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,97 +1,10 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
-  },
-  {
-    "id": "cordova-plugin-inappbrowser.inappbrowser",
-    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-    "pluginId": "cordova-plugin-inappbrowser",
-    "clobbers": [
-      "cordova.InAppBrowser.open",
-      "window.open"
-    ]
-  },
-  {
-    "id": "cordova-plugin-splashscreen.SplashScreen",
-    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-    "pluginId": "cordova-plugin-splashscreen",
-    "clobbers": [
-      "navigator.splashscreen"
-    ]
-  },
-  {
-    "id": "cordova-plugin-ionic.common",
-    "file": "plugins/cordova-plugin-ionic/www/common.js",
-    "pluginId": "cordova-plugin-ionic",
-    "clobbers": [
-      "IonicCordova"
-    ],
-    "runs": true
-  },
-  {
-    "id": "cordova-plugin-ionic-keyboard.keyboard",
-    "file": "plugins/cordova-plugin-ionic-keyboard/www/android/keyboard.js",
-    "pluginId": "cordova-plugin-ionic-keyboard",
-    "clobbers": [
-      "window.Keyboard"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local",
-      "plugin.notification.local"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
-    "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
-    "pluginId": "de.appplant.cordova.plugin.local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "mx.ferreyra.callnumber.CallNumber",
-    "file": "plugins/mx.ferreyra.callnumber/www/CallNumber.js",
-    "pluginId": "mx.ferreyra.callnumber",
-    "clobbers": [
-      "call"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-app-event": "1.2.1",
-  "cordova-plugin-device": "2.0.1",
-  "cordova-plugin-inappbrowser": "2.0.2",
-  "cordova-plugin-splashscreen": "5.0.2",
-  "cordova-plugin-ionic": "4.1.7",
-  "cordova-plugin-ionic-keyboard": "2.0.5",
-  "cordova-plugin-ionic-webview": "1.1.16",
-  "cordova-plugin-whitelist": "1.3.3",
-  "de.appplant.cordova.plugin.local-notification": "0.8.5",
-  "mx.ferreyra.callnumber": "0.0.2",
-  "cordova-android-support-gradle-release": "1.4.4"
+  "cordova-android-support-gradle-release": "1.4.4",
+  "cordova-plugin-app-event": "1.2.1"
 };
 // BOTTOM OF METADATA
 });
